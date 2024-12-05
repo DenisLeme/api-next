7782a6d2eb247e76470eadeef33fa86b67f9cd46
Denis Leme <denis_leme@icloud.com>
2024-12-05 12:48:27 -0300

    feat: consumindo api em next, falta estilizar

---
+++ app/components/Header/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Procure por alguem</h1>
+        </header>
+    )
+}
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,95 +1,81 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "./components/Header/index.js"
+import ItemList from "./components/ItemList/index.js";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+    if (newUser.name) {
+      const { avatar_url, login } = newUser;
+      setCurrentUser({ avatar_url, login });
+
+      const reposData = await fetch(
+        `https://api.github.com/users/${user}/repos`,
+        console.log("teste  ",userData)
+      );
+      const newRepos = await reposData.json();
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header/>    
+        <div className="info">
+          <div>
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@username"
+           
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <button onClick={handleGetData} >
+              Procurar
+            </button>
+          </div>
+          {currentUser?.name && (
+            <>
+              <div className="perfil">
+                <Image
+                  src={currentUser.avatar_url}
+                  width={100}
+                  height={100}
+          
+                  alt="imagem de perfil"
+                />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{user}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+              <hr />
+            </>
+          )}
+          {repos?.length && (
+            <div>
+              <h4 className="repositorio">Repositórios</h4>
+              {repos.map((repo) => (
+                <ItemList
+                  key={repo.id}
+                  title={repo.name}
+                  description={repo.description}
+                />
+              ))}
+            </div>
+          )}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      
     </div>
   );
 }
